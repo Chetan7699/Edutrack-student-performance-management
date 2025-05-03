556bf5ccb8f9508f34719f9da15756daeea8c3a0
Chetan Sachin Deshmukh <88480159+Chetan7699@users.noreply.github.com>
2025-05-03 11:44:18 +0530

    Update DBInfo.java
    Enter Your username and password for oracle database

--- EduTrack-Student-Performance-Management/src/main/java/studentmanagement/db/DBInfo.java
+++ EduTrack-Student-Performance-Management/src/main/java/studentmanagement/db/DBInfo.java
@@ -4,7 +4,7 @@ public interface DBInfo
 {
      String driver="oracle.jdbc.driver.OracleDriver";
      String url="jdbc:oracle:thin:@localhost:1521:orcl";
-     String uname="chetan";
-     String password="Chetan5332";
+     String uname="uname";
+     String password="password";
      
 }
